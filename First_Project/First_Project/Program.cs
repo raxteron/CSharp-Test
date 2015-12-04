75532fe8043cf382ed20801bc7cb023b8c910651
Radko Donchev <radko0615@gmail.com>
2015-12-04 22:50:57 +0200

    Създаване на първа програма

---
+++ First_Project/First_Project/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace First_Project
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Console.WriteLine ("Hello World!");
+			Console.ReadKey ();
+		}
+	}
+}
